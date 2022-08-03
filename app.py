b1edeee0a5f6b66d29fcfe86add45967bf40c691
kekubezos <keknorgbey@yahoo.com>
2022-08-03 11:15:33 +0000

    Interpolation with Jinja2

--- app.py
+++ app.py
@@ -2,7 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+kwargs = {
+    "name": 'Kekeli',
+    "template_name": 'Jinja2',
+    "favorites": "games",
+    "first_game": "Tomb Raider",
+    "second_game": "Gears of War 5",
+    "first_language": "Python",
+    "second_language": "JavaScript",
+    "first_interest": "Software Engineering",
+    "second_interest":"Cloud DevOps"
+}
+
 
 @app.route('/')
 def welcome():
-    return render_template('index.html', name='Kekeli', template_name='Jinja2')
+    return render_template('index.html', **kwargs)
